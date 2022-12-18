c5cb7b324dd1ddb82848bb161ca8e252fec24d6b
article-i <artii@foxmail.com>
2022-12-18 22:56:23 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -92,10 +92,10 @@ function getYesterday(cond: string): Promise<any> {
 }
 
 function onToP1() {
-  window.scrollTo(0, 3400*0.75)
+  window.scrollTo(0, 3400)
 }
 function onToP2() {
-  window.scrollTo(0, 2700*0.75)
+  window.scrollTo(0, 2700)
 }
 
 export default function HomePage() {
