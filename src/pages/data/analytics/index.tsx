8bc9a987cf816d0e879f86939d7b032e2d5bb179
article-i <artii@foxmail.com>
2022-12-22 23:34:21 +0800

    Update index.tsx

--- src/pages/data/analytics/index.tsx
+++ src/pages/data/analytics/index.tsx
@@ -1227,7 +1227,7 @@ export default function Page() {
 
       const nutbalance = await nutContract.balanceOf(addressPool);
       const nutbalanceCFX:any = new Drip(nutbalance).toCFX();
-      setTotalEmissionNUT(nutbalanceCFX.toString());
+      setTotalEmissionNUT((300000-nutbalanceCFX).toString());
 
       const confluxscanData = await axios.get(
         "https://www.confluxscan.net/stat/tokens/by-address?address=cfx%3Aacg158kvr8zanb1bs048ryb6rtrhr283ma70vz70tx&fields=iconUrl&fields=transferCount&fields=price&fields=totalPrice&fields=quoteUrl"
