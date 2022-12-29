69e0c5065f19a6510d0c35ef96745be5bec4b875
article-i <artii@foxmail.com>
2022-12-29 21:58:30 +0800

    Update index.tsx

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -130,8 +130,7 @@ export default function Layout() {
                   </a>
                 </div>
                 <div style={{color:"#fff",fontSize: "22px"}}>Audited by Hacken</div>
-                <div style={{color:"#fff",fontSize: "12px"}}>---<----------->---</div>
-                <div style={{color:"#fff",fontSize: "18px"}}>2022-2023 Nucleon. All Rights Reserved.</div>
+                <div style={{hight:"32px",color:"#fff",fontSize: "16px"}}>2022-2023 Nucleon. All Rights Reserved.</div>
               </Col>
               <Col span={4} style={{ display: "none" }}>
                 <div className={styles.t}>Information</div>
