e7023fe8b4cf74c6a0a85eb80b5eeeffae98a27d
article-i <artii@foxmail.com>
2022-12-30 12:53:46 +0800

    Update index.tsx

--- src/pages/data/pools/index.tsx
+++ src/pages/data/pools/index.tsx
@@ -610,7 +610,8 @@ export default function Page() {
         </div>
         <div className={style.box2}>
           <Row style={{ padding: "10px 20px 5px" }}>
-            <Col span={3}>{t("pools.PoolName")}</Col>
+            <Col span={1}>{t(" ")}</Col>
+            <Col span={2}>{t("pools.PoolName")}</Col>
             <Col span={2}>{t("pools.APR")}</Col>
             <Col span={3}>{t("pools.TotalLiquidity")}</Col>
             <Col span={3}>LPs in Pool</Col>
