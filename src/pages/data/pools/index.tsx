c3cd6ef00898f7bcbb7f1372b7032b1a2ddf0170
article-i <artii@foxmail.com>
2023-01-30 11:17:13 +0800

    Update index.tsx

--- src/pages/data/pools/index.tsx
+++ src/pages/data/pools/index.tsx
@@ -815,7 +815,7 @@ export default function Page() {
       // console.log(Drip(nutPerBlock).toCFX());
       setMynut(Drip(mynut.toString()).toCFX().toString());
       const confluxscanData = await axios.get(
-        "https://www.confluxscan.net/stat/tokens/by-address?address=cfx%3Aacg158kvr8zanb1bs048ryb6rtrhr283ma70vz70tx&fields=iconUrl&fields=transferCount&fields=price&fields=totalPrice&fields=quoteUrl"
+        "https://www.confluxscan.io/stat/tokens/by-address?address=cfx%3Aacg158kvr8zanb1bs048ryb6rtrhr283ma70vz70tx&fields=iconUrl&fields=transferCount&fields=price&fields=totalPrice&fields=quoteUrl"
       );
       const data = confluxscanData.data.data;
       const cfxprice = data.price;
